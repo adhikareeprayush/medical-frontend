c57660afcfe38d35b1a6fd52f55b97c517dd86f6
Sujan Ghimire <ghimire.amrit147@gmail.com>
2025-05-27 21:22:30 +0545

    tailwind css aligned with plugin

--- src/components/landing/PageBanner.jsx
+++ src/components/landing/PageBanner.jsx
@@ -1,40 +1,40 @@
-import { useLocation } from "react-router-dom";
-import serviceBanner from "../../assets/images/banner/serviceBanner.png";
-import aboutBanner from "../../assets/images/banner/aboutBanner.png";
-import blogBanner from "../../assets/images/banner/blogBanner.png";
-import doctorBanner from "../../assets/images/banner/doctorBanner.png";
-import contactBanner from "../../assets/images/banner/contactBanner.png";
-import appointmentBanner from "../../assets/images/banner/appointmentBanner.png";
+import { useLocation } from 'react-router-dom';
+import serviceBanner from '../../assets/images/banner/serviceBanner.png';
+import aboutBanner from '../../assets/images/banner/aboutBanner.png';
+import blogBanner from '../../assets/images/banner/blogBanner.png';
+import doctorBanner from '../../assets/images/banner/doctorBanner.png';
+import contactBanner from '../../assets/images/banner/contactBanner.png';
+import appointmentBanner from '../../assets/images/banner/appointmentBanner.png';
 
 const bannerConfig = {
-  "/services": {
-    title: "Our Services",
-    subtitle: "Home / Services",
+  '/services': {
+    title: 'Our Services',
+    subtitle: 'Home / Services',
     backgroundImage: serviceBanner,
   },
-  "/about": {
-    title: "About Us",
-    subtitle: "Home / About",
+  '/about': {
+    title: 'About Us',
+    subtitle: 'Home / About',
     backgroundImage: aboutBanner,
   },
-  "/contact": {
-    title: "Contact Us",
-    subtitle: "Home / Contact",
+  '/contact': {
+    title: 'Contact Us',
+    subtitle: 'Home / Contact',
     backgroundImage: contactBanner,
   },
-  "/news": {
-    title: "News & Updates",
-    subtitle: "Home / News",
+  '/news': {
+    title: 'News & Updates',
+    subtitle: 'Home / News',
     backgroundImage: blogBanner,
   },
-  "/appointment": {
-    title: "Appointment",
-    subtitle: "Home / Appointment",
+  '/appointment': {
+    title: 'Appointment',
+    subtitle: 'Home / Appointment',
     backgroundImage: appointmentBanner,
   },
-  "/doctor": {
-    title: "Appointment",
-    subtitle: "Home / Appointment",
+  '/doctor': {
+    title: 'Appointment',
+    subtitle: 'Home / Appointment',
     backgroundImage: doctorBanner,
   },
 };
@@ -43,39 +43,37 @@ const PageBanner = () => {
   const location = useLocation();
   const pathname = location.pathname;
 
-  if (pathname === "/") {
+  if (pathname === '/') {
     return null;
   }
   const banner = bannerConfig[pathname];
 
   return (
-    <div className="w-full h-[250px] flex flex-col">
+    <div className="flex h-[250px] w-full flex-col">
       <div
         className={
-          "relative w-full h-full flex items-center text-center text-white overflow-hidden "
+          'relative flex h-full w-full items-center overflow-hidden text-center text-white'
         }
       >
         {banner.backgroundImage && (
           <img
             src={banner.backgroundImage}
             alt="Banner Background"
-            className="absolute inset-0 z-0 w-full h-full object-cover "
+            className="absolute inset-0 z-0 h-full w-full object-cover"
           />
         )}
-        <div className="absolute w-[239px] h-[239px] -top-[139px] -left-[139px] rounded-full bg-secondary/30 z-10" />
-        <div className="absolute w-[239px] h-[239px] !bottom-[-139px] !right-[-139px] rounded-full bg-accent/30 z-10" />
-        <section className="relative z-10 w-full flex flex-col items-start gap-0 ">
-          <p className="font-body2 text-primary ">
-            {banner.subtitle || "Home"}
-          </p>
+        <div className="bg-secondary/30 absolute -top-[139px] -left-[139px] z-10 h-[239px] w-[239px] rounded-full" />
+        <div className="bg-accent/30 absolute !right-[-139px] !bottom-[-139px] z-10 h-[239px] w-[239px] rounded-full" />
+        <section className="relative z-10 flex w-full flex-col items-start gap-0">
+          <p className="font-body2 text-primary">{banner.subtitle || 'Home'}</p>
           <h1 className="font-display1 text-primary">{banner.title}</h1>
         </section>
-        <div className="absolute inset-0 bg-white/50 z-0" />
+        <div className="absolute inset-0 z-0 bg-white/50" />
       </div>
-      <div className="w-full flex h-[8px] z-10 ">
-        <div className="h-full flex-1 bg-accent " />
-        <div className="h-full flex-3 bg-primary " />
-        <div className="h-full flex-1 bg-secondary " />
+      <div className="z-10 flex h-[8px] w-full">
+        <div className="bg-accent h-full flex-1" />
+        <div className="bg-primary h-full flex-3" />
+        <div className="bg-secondary h-full flex-1" />
       </div>
     </div>
   );
--- src/components/landing/ServiceCard.jsx
+++ src/components/landing/ServiceCard.jsx
@@ -1,32 +1,31 @@
-import { ArrowRight } from "lucide-react";
-import bandageIcon from "../../assets/icons/bandage.svg";
+import { ArrowRight } from 'lucide-react';
+import bandageIcon from '../../assets/icons/bandage.svg';
 
 const ServiceCard = ({ service }) => {
   return (
-    <div className="w-full lg:w-[300px] h-[552px] group relative cursor-pointer">
+    <div className="group relative h-[552px] w-full cursor-pointer lg:w-[300px]">
       <div className="relative h-[300px]">
-        <div className="absolute h-full w-full group-hover:opacity-100 opacity-0 rounded-tl-md rounded-tr-md bg-primary/80 duration-300" />
+        <div className="bg-primary/80 absolute h-full w-full rounded-tl-md rounded-tr-md opacity-0 duration-300 group-hover:opacity-100" />
         <img
           src={service.image}
           alt={service.title}
-          className={`rounded-tl-md rounded-tr-md object-cover w-full h-full transition-all duration-300 `}
+          className={`h-full w-full rounded-tl-md rounded-tr-md object-cover transition-all duration-300`}
         />
-        <div className="absolute w-[80px] h-[80px] flex flex-col justify-center items-center -bottom-[32px] right-[24px] group-hover:bottom-[50%] group-hover:right-[50%] group-hover:translate-x-1/2 group-hover:translate-y-1/2 bg-primary rounded-full duration-300">
+        <div className="bg-primary absolute right-[24px] -bottom-[32px] flex h-[80px] w-[80px] flex-col items-center justify-center rounded-full duration-300 group-hover:right-[50%] group-hover:bottom-[50%] group-hover:translate-x-1/2 group-hover:translate-y-1/2">
           <img src={bandageIcon} alt="icon" width={26} height={26} />
         </div>
       </div>
 
-      <div className="pb-4 pt-5 px-3 bg-white flex flex-col gap-2 border-[1.5px] border-t-0 rounded-bl-md rounded-br-md border-gray-200 ">
-        <h3 className="text-lg font-title text-primary">Free Checkup</h3>
-        <p className="text-gray-600 font-body line-clamp-4">
+      <div className="flex flex-col gap-2 rounded-br-md rounded-bl-md border-[1.5px] border-t-0 border-gray-200 bg-white px-3 pt-5 pb-4">
+        <h3 className="font-title text-primary text-lg">Free Checkup</h3>
+        <p className="font-body line-clamp-4 text-gray-600">
           {service.description}
         </p>
         <a
           href={service.link}
-          className="text-blue-600 font-body inline-flex items-center hover:underline "
+          className="font-body inline-flex items-center text-blue-600 hover:underline"
         >
-          Learn More{" "}
-          <ArrowRight color="black" size={20} className="ml-[4px] " />
+          Learn More <ArrowRight color="black" size={20} className="ml-[4px]" />
         </a>
       </div>
     </div>
