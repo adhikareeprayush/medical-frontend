3ae2de3a83f0ad57e3c266f43f71ccd41db0a2e8
Sujan Ghimire <ghimire.amrit147@gmail.com>
2025-05-29 15:19:02 +0545

    news page done

--- src/routes/landing/news.jsx
+++ src/routes/landing/news.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import NewsCard from '../../components/landing/NewsCard';
+import PageBanner from '../../components/landing/PageBanner';
+import newsBanner from '../../assets/images/banner/newsBanner.png';
+import mockNews from '../../news.json';
 const news = () => {
   return (
-    <div>news landing</div>
-  )
-}
+    <>
+      <PageBanner subtitle="News" title="News" backgroundImage={newsBanner} />
+      <section className="flex w-full py-5">
+        <div className="flex flex-3 flex-col gap-2">
+          {mockNews.map((news) => (
+            <NewsCard key={news.id} news={news} />
+          ))}
+        </div>
+        <div className="flex-1"></div>
+      </section>
+    </>
+  );
+};
 
-export default news
+export default news;
