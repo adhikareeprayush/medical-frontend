3de52ca2cfffd66da706e5a0ababc41586930dad
Sujan Ghimire <ghimire.amrit147@gmail.com>
2025-06-02 23:08:51 +0545

    scrollToTop

--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from 'react-router-dom';
 import Header from '../components/landing/Header';
 import Nav from '../components/landing/Nav';
 import Footer from '../components/landing/Footer';
+import ScrollToTop from '../components/common/ScrollToTop';
 
 const LandingLayout = () => {
   return (
     <div>
+      <ScrollToTop />
       <Header />
       <Nav />
       <Outlet />
