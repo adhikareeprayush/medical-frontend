b5255a951461b01fa4c970e3044d911090bf1f3d
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-06-03 11:14:32 +0545

    packages page added

--- src/routes/landing/Packages.jsx
+++ src/routes/landing/Packages.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import PackagesComp from '../../components/landing/PackagesComp';
+import Data from '../../data/packages.json';
+import PageBanner from '../../components/landing/PageBanner';
+import PackageBanner from '../../assets/images/hospital_banner/hospital_banner.jpg';
+import ContactSection from '../../components/landing/ContactSection';
 
 const Packages = () => {
   return (
-    <div className="grid-col-4 md:grid-col-2 sm:grid-col-1 grid h-full w-full justify-center">
-      <PackagesComp />
-    </div>
+    <>
+      <PageBanner
+        subtitle="Package"
+        title={Packages}
+        backgroundImage={PackageBanner}
+      />
+      <section className="relative flex h-full w-full flex-col items-center justify-center py-4">
+        <div className="grid sm:grid-cols-2 sm:gap-6 lg:grid-cols-3 lg:gap-6">
+          {Data.map((test, index) => (
+            <PackagesComp key={index} testData={test} />
+          ))}
+        </div>
+      </section>
+      <ContactSection />
+    </>
   );
 };
 
