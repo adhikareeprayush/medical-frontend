2628bb60eeea7f1e81e7575e3c17b09a6ae462eb
Biswash <koiralabiswash123@gmail.com>
2025-06-07 20:17:27 +0545

    clical department section added

---
+++ src/data/department.js
@@ -0,0 +1,138 @@
+import GeneralMedicine from '../assets/images/clinicalDepartment/General Medicine.png';
+import Cardiology from '../assets/images/clinicalDepartment/Cardiology.png';
+import Neuroscience from '../assets/images/clinicalDepartment/Neuroscience.png';
+import Pediatrics from '../assets/images/clinicalDepartment/Paediatrics.png';
+import Orthopedic from '../assets/images/clinicalDepartment/Orthopedic.png';
+import Ophthalmology from '../assets/images/clinicalDepartment/opthalmology.png';
+import ENT from '../assets/images/clinicalDepartment/ENT.png';
+import Dermatology from '../assets/images/clinicalDepartment/Dermatology.png';
+import Psychiatry from '../assets/images/clinicalDepartment/pshyciatry.png';
+import Urology from '../assets/images/clinicalDepartment/Urology.png';
+import Oncology from '../assets/images/clinicalDepartment/Oncology.png';
+import Nephrology from '../assets/images/clinicalDepartment/Nephrology.png';
+import Rheumatology from '../assets/images/clinicalDepartment/Rheumatology.png';
+import Anesthesiology from '../assets/images/clinicalDepartment/Anesthesiology.png';
+import Gyna from '../assets/images/clinicalDepartment/Gyna.png';
+import GeneralSurgery from '../assets/images/clinicalDepartment/General Surgery.png';
+import Nutrition from '../assets/images/clinicalDepartment/Nutrition.png';
+import VascularSurgery from '../assets/images/clinicalDepartment/vascular surgery.png';
+import TeethSurgery from '../assets/images/clinicalDepartment/teeth surgery.png';
+
+const clinicalDepartments = [
+  {
+    id: 1,
+    name: 'General Medicine',
+    nepali_name: 'सामान्य चिकित्सा',
+    image: GeneralMedicine,
+  },
+  {
+    id: 2,
+    name: 'Cardiology',
+    nepali_name: 'हृदय रोग',
+    image: Cardiology,
+  },
+  {
+    id: 3,
+    name: 'Neuroscience',
+    nepali_name: 'स्नायु विज्ञान',
+    image: Neuroscience,
+  },
+  {
+    id: 4,
+    name: 'Pediatrics',
+    nepali_name: 'बाल चिकित्सा',
+    image: Pediatrics,
+  },
+  {
+    id: 5,
+    name: 'Orthopedic Surgery',
+    nepali_name: 'हाड जोर्नी विभाग',
+    image: Orthopedic,
+  },
+  {
+    id: 6,
+    name: 'Ophthalmology',
+    nepali_name: 'नेत्र विज्ञान',
+    image: Ophthalmology,
+  },
+  {
+    id: 7,
+    name: 'ENT',
+    nepali_name: 'कान, नाक र घाँटी',
+    image: ENT,
+  },
+  {
+    id: 8,
+    name: 'Dermatology',
+    nepali_name: 'छाला विज्ञान',
+    image: Dermatology,
+  },
+  {
+    id: 9,
+    name: 'Psychiatry',
+    nepali_name: 'मानसिक स्वास्थ्य',
+    image: Psychiatry,
+  },
+  {
+    id: 10,
+    name: 'Urology',
+    nepali_name: 'मूत्र विज्ञान',
+    image: Urology,
+  },
+  {
+    id: 13,
+    name: 'Oncology',
+    nepali_name: 'क्यान्सर विज्ञान',
+    image: Oncology,
+  },
+  {
+    id: 14,
+    name: 'Nephrology',
+    nepali_name: 'मृगौला विज्ञान',
+    image: Nephrology,
+  },
+  {
+    id: 15,
+    name: 'Rheumatology',
+    nepali_name: 'वातविज्ञान',
+    image: Rheumatology,
+  },
+  {
+    id: 16,
+    name: 'Anesthesiology',
+    nepali_name: 'स्नायुशल्यक्रिया',
+    image: Anesthesiology,
+  },
+  {
+    id: 17,
+    name: 'Gyanecology/ Obstetrics',
+    nepali_name: 'स्त्री रोग/ प्रसूति',
+    image: Gyna,
+  },
+  {
+    id: 18,
+    name: 'General Surgery',
+    nepali_name: 'सामान्य शल्यक्रिया',
+    image: GeneralSurgery,
+  },
+  {
+    id: 19,
+    name: 'Nutrition and Dietetics',
+    nepali_name: 'पोषण र आहार विज्ञान',
+    image: Nutrition,
+  },
+  {
+    id: 20,
+    name: 'Cardio-thoracic & Vascular Surgery',
+    nepali_name: 'हृदय-फोक्सो र रक्तनली शल्यक्रिया',
+    image: VascularSurgery,
+  },
+  {
+    id: 21,
+    name: 'Cleft & Craniomaxillofacial Surgery',
+    nepali_name: 'ओठ-तालु र खोपडी-माथिल्लो अनुहार शल्यक्रिया',
+    image: TeethSurgery,
+  },
+];
+
+export default clinicalDepartments;
