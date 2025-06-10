1d4ba5038b8bff99e4debbd95ab4b0287f6875a9
adhikareeprayush <adhikareeprayush@gmail.com>
2025-06-10 22:46:02 +0545

    updated...

--- src/routes/landing/Packages.jsx
+++ src/routes/landing/Packages.jsx
@@ -3,8 +3,24 @@ import Data from '../../data/packages.json';
 import PageBanner from '../../components/landing/PageBanner';
 import PackageBanner from '../../assets/images/hospital_banner/hospital_banner.jpg';
 import ContactSection from '../../components/landing/ContactSection';
+import { getAllPackages } from '../../utils/api';
+import { useState, useEffect } from 'react';
 
 const Packages = () => {
+  const [packages, setPackages] = useState([]);
+  const fetchPackages = async () => {
+    try {
+      const response = await getAllPackages();
+      console.log('Fetched packages:', response.data.data);
+      setPackages(response.data.data);
+    } catch (error) {
+      console.error('Error fetching packages:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPackages();
+  }, []);
   return (
     <>
       <PageBanner
@@ -13,14 +29,14 @@ const Packages = () => {
         backgroundImage={PackageBanner}
       />
 
-      <section className="container mx-auto px-4 py-8 md:py-12 xl:px-[140px]">
+      <section className="container mx-auto px-4 py-8 md:py-12 xl:px-8">
         <h1 className="text-primary mb-6 text-center text-4xl font-bold lg:text-5xl">
           Our Packages
         </h1>
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-5">
-          {Data.map((test, index) => (
-            <div key={index} className="flex h-full">
-              <PackagesComp testData={test} />
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-4">
+          {packages.map((pkg) => (
+            <div key={pkg.id} className="flex h-full">
+              <PackagesComp testData={pkg} />
             </div>
           ))}
         </div>
