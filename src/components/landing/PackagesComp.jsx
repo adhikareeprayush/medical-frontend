87ab744aacc1617941d1c41c3b39fffa85ca79b1
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-06-12 23:27:35 +0545

    data added

--- src/components/landing/PackagesComp.jsx
+++ src/components/landing/PackagesComp.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const PackagesComp = ({ testData }) => {
   return (
-    <div className="flex min-h-[400px] min-w-[300px] flex-col rounded-2xl shadow-2xl transition-shadow duration-300 hover:cursor-pointer">
+    <div className="flex min-h-[400px] min-w-[350px] flex-col rounded-2xl shadow-2xl transition-shadow duration-300 hover:cursor-pointer">
       <div className="bg-primary rounded-t-2xl px-3 py-3 text-center">
         <h1 className="text-lg font-bold text-white sm:text-lg md:text-xl">
           {testData.title}
         </h1>
       </div>
 
-      <div className="flex flex-grow flex-col border-b border-gray-300">
+      <div className="flex flex-grow flex-col justify-end border-b border-gray-300">
         <div className="scrollbar-none hide-scrollbar max-h-[300px] overflow-y-auto p-4 sm:max-h-[280px]">
           <ul className="space-y-2">
             {testData.checks.split(',').map((check, idx) => (
