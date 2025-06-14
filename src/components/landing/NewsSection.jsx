eeca234f7df9c6a63a020837e68fc93363938868
Biswash <koiralabiswash123@gmail.com>
2025-06-14 22:23:14 +0545

    updated

--- src/components/landing/NewsSection.jsx
+++ src/components/landing/NewsSection.jsx
@@ -23,28 +23,16 @@ const NewsSection = () => {
   return (
     <section className="my-24 flex w-full flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
       {/* Heading */}
-<<<<<<< HEAD
-      <div className="mb-7 flex flex-col gap-3 text-center">
-        <h1 className="text-secondary text-base font-bold tracking-widest uppercase sm:text-lg md:text-xl">
-          Better Information, Better Health
-        </h1>
-        <p className="text-primary font-display2 lg:font-display1">News</p>
-=======
       <div className="mb-10 text-center">
         <h1 className="text-secondary mb-3 text-sm font-bold tracking-widest uppercase sm:text-base md:text-lg">
           Better Information, Better Health
         </h1>
         <p className="text-primary font-display1 text-2xl sm:text-3xl">News</p>
->>>>>>> 693f4030d23270b99c1f88900b7dd46f4013d548
       </div>
 
       {/* Grid of news */}
       <div className="grid w-full max-w-6xl grid-cols-1 gap-4 sm:grid-cols-2">
-<<<<<<< HEAD
-        {NewsData.map((news) => (
-=======
         {recentNews.map((news) => (
->>>>>>> 693f4030d23270b99c1f88900b7dd46f4013d548
           <div
             key={news.id}
             className="rounded-md bg-white shadow-sm transition-shadow duration-300 hover:shadow-md"
@@ -63,9 +51,6 @@ const NewsSection = () => {
               <div className="flex w-full flex-col justify-between p-2 lg:w-3/5 lg:p-3">
                 <div>
                   <p className="text-secondary mb-1 text-sm font-medium tracking-wider">
-<<<<<<< HEAD
-                    {news.date} | By {news.author}
-=======
                     {new Date(news.createdAt).toLocaleDateString('en-US', {
                       day: '2-digit',
                       month: 'long',
@@ -74,7 +59,6 @@ const NewsSection = () => {
                     |{' '}
                     {news.source?.match(/https?:\/\/(?:www\.)?([^./]+)/)?.[1] ??
                       'Unknown'}
->>>>>>> 693f4030d23270b99c1f88900b7dd46f4013d548
                   </p>
                   <h3 className="mb-3 text-lg leading-snug font-semibold text-black">
                     {news.title}
