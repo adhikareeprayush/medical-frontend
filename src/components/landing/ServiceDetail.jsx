e2c626180ec35e2662c15bc36bf86f60cdf34f6a
Sujan Ghimire <ghimire.amrit147@gmail.com>
2025-06-19 23:59:08 +0545

    services

--- src/components/landing/ServiceDetail.jsx
+++ src/components/landing/ServiceDetail.jsx
@@ -16,19 +16,17 @@ const ServiceDetail = ({ service }) => {
       </h1>
       {Array.isArray(service?.points) || typeof service?.points === 'string' ? (
         <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
-          {(typeof service?.points === 'string'
-            ? JSON.parse(
-                service.points
-                  .replace(/'''/g, '"') // replace triple single quotes with double quotes
-                  .replace(/'/g, '"'), // replace remaining single quotes with double quotes
-              )
-            : service.points
-          )?.map((point, index) => (
-            <div key={index} className="flex items-center gap-1">
-              <span className="bg-secondary h-2 w-2 rounded-full"></span>
-              <span className="text-base text-gray-800">{point}</span>
-            </div>
-          ))}
+          {service.points
+            ?.replace(/^\[|\]$/g, '') // remove [ and ]
+            .split(',')
+            .map((point, idx) => (
+              <div key={idx} className="flex items-center gap-1">
+                <span className="bg-secondary h-2 w-2 rounded-full"></span>
+                <span className="text-base text-gray-800">
+                  {point.replace(/['"]/g, '').trim()}
+                </span>
+              </div>
+            ))}
         </div>
       ) : null}
       <p className="font-body">{service.description}</p>
