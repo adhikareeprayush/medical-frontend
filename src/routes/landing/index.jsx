d2a8fdba501a65f4bc3a5f115d9e994c77c8f706
Biswash <koiralabiswash123@gmail.com>
2025-06-19 21:38:42 +0545

    add loading component

--- src/routes/landing/index.jsx
+++ src/routes/landing/index.jsx
@@ -1,4 +1,5 @@
 import Section from '../../components/landing/Section';
+import React, { useEffect, useState } from 'react';
 import Hero from '../../components/landing/Hero';
 import WelcomeSec from '../../components/landing/WelcomeSec';
 import DoctorSec from '../../components/landing/DoctorSec';
@@ -6,18 +7,33 @@ import OurDocSec from '../../components/landing/OurDocSec';
 import NewsSection from '../../components/landing/NewsSection';
 import ContactSection from '../../components/landing/ContactSection';
 import ClinicalDepartment from '../../components/landing/ClinicalDepartment';
+import LoadingComp from '../../components/common/LoadingComp';
 
 const index = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Simulate a loading time or wait for an API call
+    const timer = setTimeout(() => setLoading(false), 1000); // 3 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
+      {loading ? (
+        <LoadingComp />
+      ) : (
+        <div>
+          <Hero />
+          <WelcomeSec />
+          <DoctorSec />
+          <ClinicalDepartment />
+          <OurDocSec />
+          <NewsSection />
+          <ContactSection />
+        </div>
+      )}
       {/* <StickySidebar /> */}
-      <Hero />
-      <WelcomeSec />
-      <DoctorSec />
-      <ClinicalDepartment />
-      <OurDocSec />
-      <NewsSection />
-      <ContactSection />
+
       {/* <Section /> */}
     </>
   );
