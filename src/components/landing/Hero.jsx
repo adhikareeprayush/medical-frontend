afd692712c98e8512e33fc523331ebb9dad5e46d
adhikareeprayush <adhikareeprayush@gmail.com>
2025-06-19 19:54:57 +0545

    footer links fixed

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
       <div className="absolute inset-0 z-10 bg-black opacity-60" />
 
       {/* Hero Text */}
-      <div className="z-20 px-4 text-center text-white">
+      <div className="z-20 px-4 text-center text-[#fff] shadow">
         <h1 className="text-5xl font-bold md:text-7xl">
           Welcome to Nisasrga Hospital
         </h1>
