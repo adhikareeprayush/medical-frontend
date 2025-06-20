2531f5afb504d8018815ae2e5a2b85010e9708ee
Sujan Ghimire <ghimire.amrit147@gmail.com>
2025-06-20 15:36:01 +0545

    home changes

--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/landing/Header';
 import Nav from '../components/landing/Nav';
 import Footer from '../components/landing/Footer';
@@ -6,13 +6,21 @@ import ScrollToTop from '../components/common/ScrollToTop';
 import StickySidebar from '../components/common/StickySidebar';
 
 const LandingLayout = () => {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
   return (
     <div className="relative flex">
       {/* Main content area */}
       <div className="min-w-0 flex-1">
         <ScrollToTop />
-        <Header />
-        <Nav />
+        <div
+          className={`z-50 flex w-full flex-col gap-1 ${
+            isHome ? 'absolute' : 'relative'
+          }`}
+        >
+          <Header />
+          <Nav />
+        </div>
         <Outlet />
         <Footer />
       </div>
