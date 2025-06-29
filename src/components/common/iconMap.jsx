0321b1cf23c61f0d00820bb4b8d9f28ca4a0a060
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-06-29 14:51:21 +0545

    icons added

--- src/components/common/iconMap.jsx
+++ src/components/common/iconMap.jsx
@@ -1,11 +1,22 @@
 import {
   FaStethoscope,
+  FaClinicMedical,
   FaBaby,
   FaTooth,
   FaHeartbeat,
+  FaXRay,
 } from 'react-icons/fa';
+import { TbPhysotherapist } from 'react-icons/tb';
 import { GiMedicines } from 'react-icons/gi';
-
+import { GrEmergency } from 'react-icons/gr';
+import {
+  dermatology,
+  Genereal_medicine,
+  General_Surgery,
+  Neuroscience,
+  urology,
+  vascular_surgery,
+} from '../../assets/images/clinicalDepartment/Dermatology.png';
 
 const iconMap = {
   stethoscope: FaStethoscope,
@@ -13,6 +24,16 @@ const iconMap = {
   tooth: FaTooth,
   heartbeat: FaHeartbeat,
   medicines: GiMedicines,
+  emergency: GrEmergency,
+  skin: dermatology,
+  icu: FaClinicMedical,
+  pharmacy: Genereal_medicine,
+  ot: General_Surgery,
+  neurosurgery: Neuroscience,
+  ventilator: vascular_surgery,
+  radiology: FaXRay,
+  physiotherapy: TbPhysotherapist,
+  hepatology: urology,
 };
 
 export default iconMap;
